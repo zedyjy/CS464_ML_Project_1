d400d225aed7f31ca5842f8eb4b48e13abe67784
zedyjy <doga.dellal@ug.bilkent.edu.tr>
2024-11-16 00:22:07 +0300

    train code

--- train.py
+++ train.py
@@ -1,10 +1,14 @@
 from ultralytics import YOLO
 
-# Load a pre-trained model (COCO)
-model = YOLO('yolov8n.pt')  # Use 'yolov8s.pt', 'yolov8m.pt', etc., for different sizes
+# Load a pre-trained YOLOv8 model (you can use 'yolov8n.pt' for a smaller model)
+model = YOLO('yolov8n.pt')  # Options: 'yolov8n.pt', 'yolov8s.pt', etc.
 
-# Train on your dataset
-model.train(data='coco.json', epochs=50, imgsz=640, batch=8)
+# Train the model on your dataset
+model.train(data='./train_coco.yaml', epochs=50)
 
-results = model.val()
-model.predict(source='./processed/test_images/2_1040010024CD5300_tile_124.png', save=True)
+
+# Validate the model to check its performance on the validation set
+# results = model.val()
+
+# Test the model on a new image
+model.predict(source='./processed/test_images/105_104001003108D900_tile_47.png', save=True)
